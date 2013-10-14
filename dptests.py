f990f13e86d74d94d09d19eddfec545c35fcf664
Srini Seetharaman <srini.seetharaman@gmail.com>
2013-10-14 11:22:05 -0700

    Committing additional structure

--- dptests.py
+++ dptests.py
@@ -1,31 +1,42 @@
+import inspect
 
 def one_packet_ping_same_hypervisor():
+    print "Running test", inspect.stack()[0][3]
     pass
 
 def one_packet_ping_diff_hypervisor():
+    print "Running test", inspect.stack()[0][3]
     pass
 
 def ping_same_hypervisor():
+    print "Running test", inspect.stack()[0][3]
     pass
 
 def ping_diff_hypervisor():
+    print "Running test", inspect.stack()[0][3]
     pass
 
 def iperf_same_hypervisor():
+    print "Running test", inspect.stack()[0][3]
     pass
 
 def iperf_diff_hypervisor():
+    print "Running test", inspect.stack()[0][3]
     pass
 
 def arping_same_hypervisor():
+    print "Running test", inspect.stack()[0][3]
     pass
 
 def arping_diff_hypervisor():
+    print "Running test", inspect.stack()[0][3]
     pass
 
 def hping3_same_hypervisor():
+    print "Running test", inspect.stack()[0][3]
     pass
 
 def hping3_diff_hypervisor():
+    print "Running test", inspect.stack()[0][3]
     pass
 
--- test.py
+++ test.py
@@ -1,4 +1,15 @@
 import dptests
+import sys
+
+def run_test(test_name):
+    func = getattr(dptests, test_name, None)
+    assert(func),("Test %s is not defined" % test_name)
+    try:
+        return func()
+    except Exception, e:
+        return "Error: %s" % e
 
 
-
+# For now, assuming that 
+if (len(sys.argv) > 1):
+    run_test(sys.argv[1])
